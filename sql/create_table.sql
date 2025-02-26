2f87124a0c7df3a2c701075f404eae76d99e7398
Alayaya Roslin <alayayar@student.21-school.ru>
2025-02-26 17:34:35 +0300

    Add a column to store row hash

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -6,9 +6,11 @@ CREATE TABLE IF NOT EXISTS apache_logs (
     status_code INTEGER NULL,
     response_size INTEGER NULL,
     referer_url TEXT,
-    user_agent TEXT
+    user_agent TEXT,
+    row_hash VARCHAR(64) UNIQUE
 );
 
 CREATE INDEX idx_ip_address ON apache_logs (ip_address);
 CREATE INDEX idx_log_timestamp ON apache_logs (log_timestamp);
 CREATE INDEX idx_status_code ON apache_logs (status_code);
+CREATE INDEX idx_row_hash ON apache_logs (row_hash);
